91bfc1c2bd4b6dd113bbe53221ee897a104f743b
Anna Salkutsan <salkutsanann@gmail.com>
2024-09-20 19:17:01 +0300

    RefAndValType type: feat

---
+++ BankSystem.Domain/Models/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Domain.Models
+{
+    /// <summary>
+    /// Класс "Клиент"
+    /// </summary>
+    public class Client:Person
+    {
+        /// <summary>
+        /// Серия паспорта
+        /// </summary>
+        public string PassportSeries { get; set; }
+        
+        /// <summary>
+        /// Номер паспорта
+        /// </summary>
+        public string PassportNumber { get; set; }
+        
+        /// <summary>
+        /// Дата регистрации в банке
+        /// </summary>
+        public DateTime RegistrationDate { get; set; }
+
+        public Client(string passportSeries, string passportNumber, DateTime registrationDate)
+        {
+            PassportSeries = passportSeries;
+            PassportNumber = passportNumber;
+            RegistrationDate = registrationDate;
+        }
+        
+        public Client() { }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Contracts;
+using BankSystem.Domain.Models;
+
+namespace Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Практическое задание 1 "Типы значений и ссылочные типы"
+            Employee employee = new Employee
+            {
+                FirstName = "Степан",
+                LastName = "Музыченко",
+                PhoneNumber = "+373 775 21 441",
+                BirthDay = new DateTime(1995, 5, 20),
+                Position = "Менеджер"
+            };
+            Console.WriteLine($"Старый контракт: {employee.Contract}");
+           
+            UpdateEmployeeContract(employee);
+            Console.WriteLine($"Обновленный контракт: {employee.Contract}");
+            
+            Currency currency = new Currency("USD", "United States Dollar");
+            Console.WriteLine($"Старая валюта: Code = {currency.Code}, Name = {currency.Name}");
+            
+            UpdateCurrency(ref currency, "EUR", "Euro");
+            Console.WriteLine($"Обновленная валюта: Code = {currency.Code}, Name = {currency.Name}");
+            
+        }
+
+        /// <summary>
+        /// Обновление контракта сотрудника на основе его данных
+        /// </summary>
+        static void UpdateEmployeeContract(Employee employee)
+        {
+            employee.Contract = $"Контракт для {employee.FirstName} {employee.LastName}; Должность: {employee.Position}; Контактный номер: {employee.PhoneNumber}";
+        }
+
+        /// <summary>
+        /// Обновление свойств валюты
+        /// </summary>
+        static void UpdateCurrency(ref Currency currency, string newCode, string newName)
+        {
+            currency.Code = newCode;
+            currency.Name = newName;
+        }
+    }
+}
