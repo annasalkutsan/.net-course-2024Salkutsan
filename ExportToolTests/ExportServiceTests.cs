614b9389b652ef19bb6e76e9c48b6bcfd4490c83
Anna Salkutsan <salkutsanann@gmail.com>
2024-10-22 11:34:15 +0300

    Serialization JSON feat

--- ExportToolTests/ExportServiceTests.cs
+++ ExportToolTests/ExportServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using BankSystem.App.Interfaces;
+﻿using System.Text.Json;
+using BankSystem.App.Interfaces;
 using BankSystem.App.Services;
 using BankSystem.Data.EntityConfigurations;
 using BankSystem.Data.Storages;
@@ -12,7 +13,8 @@ public class ExportServiceTests
 {
     private readonly string _testDirectory = @"D:\Dex\Practic\.net-course-2024Salkutsan\ExportToolTests\TestCsv";
     private readonly string _csvFileName = "test_clients.csv";
-
+    private readonly string _jsonFileName = "test_clients.json";
+    
     private readonly IClientStorage _clientStorage;
     private readonly TestDataGenerator _dataGenerator;
     private readonly BankSystemDbContext _context;
@@ -24,9 +26,54 @@ public class ExportServiceTests
         _clientStorage = new ClientStorage(_context);
         _dataGenerator = new TestDataGenerator();
         
-        _exportService = new ExportService<Client>(_clientStorage, _context);
+        _exportService = new ExportService<Client>(_clientStorage);
     }
 
+    [Fact]
+    public void ExportToJsonPositiveTest()
+    {
+        // Arrange
+        var entities = _dataGenerator.GenerateClients(5); 
+        foreach (var entity in entities)
+        {
+            _context.Add(entity); 
+            _context.SaveChanges();
+        }
+
+        // Act
+        _exportService.ExportToJson(_testDirectory, _jsonFileName); 
+
+        // Assert
+        string fullPath = Path.Combine(_testDirectory, _jsonFileName);
+        Assert.True(File.Exists(fullPath)); 
+
+        var json = File.ReadAllText(fullPath);
+        var deserializedEntities = JsonSerializer.Deserialize<List<Client>>(json);
+
+        Assert.Equal(entities.Count, deserializedEntities.Count); 
+    }
+    
+    [Fact]
+    public void ImportFromJsonPositiveTest()
+    {
+        // Arrange
+        var entities = _dataGenerator.GenerateClients(5); 
+        string json = JsonSerializer.Serialize(entities); 
+
+        // Создание тестового JSON-файла
+        string fullPath = Path.Combine(_testDirectory, _jsonFileName);
+        Directory.CreateDirectory(_testDirectory); 
+        File.WriteAllText(fullPath, json);
+
+        // Act
+        _exportService.ImportFromJson(_testDirectory, _jsonFileName); 
+        _context.SaveChanges(); 
+        
+        // Assert
+        var importedEntities = _clientStorage.GetAll(); 
+        Assert.Equal(entities.Count, importedEntities.Count); 
+    }
+    
     [Fact]
     public void ExportClientsToCsvPositiveTest()
     {
@@ -35,11 +82,12 @@ public class ExportServiceTests
         foreach (var client in clients)
         {
             _clientStorage.Add(client);
+            _context.SaveChanges(); 
         }
 
         // Act
         _exportService.ExportToCsv(_testDirectory, _csvFileName); 
-
+        
         // Assert
         string fullPath = Path.Combine(_testDirectory, _csvFileName);
         Assert.True(File.Exists(fullPath)); 
@@ -57,7 +105,6 @@ public class ExportServiceTests
         var clients = _dataGenerator.GenerateClients(3); 
         string fullPath = Path.Combine(_testDirectory, _csvFileName);
 
-        // CSV файл с данными клиентов
         using (var writer = new StreamWriter(fullPath))
         {
             writer.WriteLine("Passport,Id,FirstName,LastName,PhoneNumber,BirthDay,CreateUtc");
@@ -69,6 +116,7 @@ public class ExportServiceTests
 
         // Act
         _exportService.ImportFromCsv(_testDirectory, _csvFileName); 
+        _context.SaveChanges(); 
 
         // Assert
         var importedClients = _clientStorage.GetAll();
